dddcb0a26552e1a6459ec0ac6836e8fd87107924
RRTRyan <tiaryanrzk@gmail.com>
2025-08-31 15:25:38 +0300

    fix: use api summary route for alerts

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,43 +3,21 @@ import { PieChart } from "../components/PieChart.tsx";
 import ExpenseList from "../components/ExpenseList.tsx";
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
-import type { Transaction } from '../components/Transaction/Types.tsx';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Dashboard() {
     const token = localStorage.getItem('accessToken')
 
     const { t } = useTranslation();
-    const [monthExpenses, setMonthExpenses] = useState<Transaction[] | null>(null)
-    const [monthIncomes, setMonthIncomes] = useState<Transaction[] | null>(null)
-    const [remainingBalance, setRemainingBalane] = useState<number>(0)
+    const [balanceAlert, setBalaceAlert] = useState<{ alert: boolean, message: string } | null>(null)
 
-    function getMonthExpenses(time: { year: number, month: number }) {
+    function checkMonthBalance() {
         try {
-            const monthStart = new Date(`${time.year}-${time.month < 10 ? ("0" + time.month) : time.month}`).toISOString()
-            const monthEnd = new Date(time.month === 12 ? `${time.year + 1}-01` : `${time.year}-${time.month < 9 ? ("0" + (time.month + 1)) : time.month + 1}`).toISOString()
-
-            fetch(`http://localhost:8080/api/expenses?start=${monthStart}&end=${monthEnd}`, {
-                headers: { Authorization: "Bearer " + token }
-            })
+            fetch("http://localhost:8080/api/summary/alerts",
+                { headers: { Authorization: "Bearer " + token } }
+            )
                 .then(res => res.json())
-                .then(res => setMonthExpenses(res))
-                .catch(rej => console.log(rej.message))
-        } catch (err: any) {
-            console.log(err.message)
-        }
-    }
-
-    function getMonthIncomes(time: { year: number, month: number }) {
-        try {
-            const monthStart = new Date(`${time.year}-${time.month < 10 ? ("0" + time.month) : time.month}`).toISOString()
-            const monthEnd = new Date(time.month >= 12 ? `${time.year + 1}-01` : `${time.year}-${time.month < 9 ? ("0" + (time.month + 1)) : time.month + 1}`).toISOString()
-
-            fetch(`http://localhost:8080/api/income?start=${monthStart}&end=${monthEnd}`, {
-                headers: { Authorization: "Bearer " + token }
-            })
-                .then(res => res.json())
-                .then(res => setMonthIncomes(res))
+                .then(res => setBalaceAlert(res))
                 .catch(rej => console.log(rej.message))
         } catch (err: any) {
             console.log(err.message)
@@ -47,13 +25,8 @@ export default function Dashboard() {
     }
 
     useEffect(() => {
-        getMonthExpenses({ year: new Date().getFullYear(), month: new Date().getMonth() })
-        getMonthIncomes({ year: new Date().getFullYear(), month: new Date().getMonth() + 1 })
-    }, [monthExpenses, monthIncomes])
-
-    useEffect(() => {
-        setRemainingBalane(((monthIncomes?.reduce((acc, income) => acc += Number(income.amount), 0) || 0) - (monthExpenses?.reduce((acc, expense) => acc += Number(expense.amount), 0) || 0)))
-    }, [monthExpenses, monthIncomes])
+        checkMonthBalance()
+    }, [])
 
     return (
         <section className={'flex'}>
@@ -64,12 +37,12 @@ export default function Dashboard() {
                     </h1>
                 </div>
                 <AnimatePresence>
-                    {(remainingBalance < 0) && (<motion.div
+                    {(balanceAlert?.alert) && (<motion.div
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         className='flex p-3 px-6 -mt-3 mb-2 items-center border border-amber-400 bg-yellow-300/40 rounded-[10px]'>
-                        <h3 className='font-medium text-gray-900 flex items-center'>You have exceeded your monthly budget by <span className='text-red-700 text-2xl pl-2'>${-remainingBalance}</span></h3>
+                        <h3 className='font-medium text-lg text-red-500 flex items-center'>{balanceAlert.message}</h3>
                     </motion.div>)}
                 </AnimatePresence>
                 <div className={'flex justify-evenly gap-6'}>
